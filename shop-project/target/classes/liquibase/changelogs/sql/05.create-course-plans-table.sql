ccea8e16c967232f084ed7293368f11b5a230571
Alex Y <balticano05@gmail.com>
2024-09-20 22:05:50 +0300

    correct 6 homework

--- shop-project/target/classes/liquibase/changelogs/sql/05.create-course-plans-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE course_plans (
-    id BIGSERIAL PRIMARY KEY,
-    lesson_count INTEGER NOT NULL CHECK (lesson_count >= 0),
-    practice_count INTEGER NOT NULL CHECK (practice_count >= 0),
-    duration INTEGER NOT NULL CHECK (duration >= 0),
-    FOREIGN KEY (id) REFERENCES courses(id) ON DELETE CASCADE
-);
